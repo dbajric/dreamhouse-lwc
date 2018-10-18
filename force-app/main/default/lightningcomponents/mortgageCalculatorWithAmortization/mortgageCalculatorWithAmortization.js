c6249c58303184a9ee904ec1c2011d8e63c6d892
Dinko Bajric <dbajric@salesforce.com>
2018-10-18 14:43:04 -0700

    Fixed intial value of principal

--- force-app/main/default/lightningcomponents/mortgageCalculatorWithAmortization/mortgageCalculatorWithAmortization.js
+++ force-app/main/default/lightningcomponents/mortgageCalculatorWithAmortization/mortgageCalculatorWithAmortization.js
@@ -1,3 +1,20 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api, track, wire } from "lwc";
+import { getRecord } from "lightning/uiRecordApi";
 
-export default class MortgageCalculatorWithAmortization extends LightningElement {}
+export default class MortgageCalculatorWithAmortization extends LightningElement {
+    @api recordId;
+    @track principal = 200000;
+
+    @wire(getRecord, { recordId: "$recordId", fields: ["Property__c.Price__c"] })
+    wiredRecord({ error, data }) {
+        if (error) {
+            showToast({
+                title: "Error loading pictures",
+                message: error.message,
+                variant: "error"
+            });
+        } else if (data) {
+            this.principal = data.fields.Price__c.value;
+        }
+    }
+}
