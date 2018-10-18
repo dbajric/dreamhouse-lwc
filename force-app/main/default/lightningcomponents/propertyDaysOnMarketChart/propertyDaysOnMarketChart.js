a94bc99103b1489932a518d40de4745ad01fa303
Dinko Bajric <dbajric@salesforce.com>
2018-10-18 13:13:35 -0700

    Use @wire on Days on Market component

--- force-app/main/default/lightningcomponents/propertyDaysOnMarketChart/propertyDaysOnMarketChart.js
+++ force-app/main/default/lightningcomponents/propertyDaysOnMarketChart/propertyDaysOnMarketChart.js
@@ -1,11 +1,43 @@
-import { LightningElement, track } from "lwc";
+import { LightningElement, api, track, wire } from "lwc";
+import { showToast } from "lightning/notificationsLibrary";
+import { getRecord } from "lightning/uiRecordApi";
+
+const fields = ["Property__c.Date_Listed__c", "Property__c.Days_On_Market__c"];
 
 export default class PropertyDaysOnMarketChart extends LightningElement {
+    @api recordId;
+
     @track daysOnMarket = 0;
     @track daysBlockClass = "days-block";
     @track barClass = "bar";
     @track barStyle = "width: 0%";
     @track formattedDateListed = "";
+
+    @wire(getRecord, { recordId: "$recordId", fields: fields })
+    wiredRecord({ error, data }) {
+        if (error) {
+            showToast({
+                title: "Error loading selected Property.",
+                message: error.message,
+                variant: "error"
+            });
+        } else if (data) {
+            let property = data.fields;
+            this.daysOnMarket = property.Days_On_Market__c.value;
+
+            let status = "green";
+            if (this.daysOnMarket > 60) {
+                status = "red";
+            } else if (this.daysOnMarket > 30) {
+                status = "orange";
+            }
+
+            this.daysBlockClass = `days-block ${status}`;
+            this.barClass = `bar ${status}`;
+            this.barStyle = `width: ${this.daysOnMarket / 90 * 100}%`;
+            this.formattedDateListed = new Date(property.Date_Listed__c.value).toLocaleString("en-US", {month: "short", year: "numeric", day: "numeric"});
+        }
+    }
 
     connectedCallback() {
         this.onPropertySelectedCallback = this.onPropertySelected.bind(this);
@@ -21,19 +53,6 @@ export default class PropertyDaysOnMarketChart extends LightningElement {
      * @param {Event} evt change event.
      */
     onPropertySelected(evt) {
-        let property = evt.detail;
-        this.daysOnMarket = property.Days_On_Market__c;
-
-        let status = "green";
-        if (this.daysOnMarket > 60) {
-            status = "red";
-        } else if (this.daysOnMarket > 30) {
-            status = "orange";
-        }
-
-        this.daysBlockClass = `days-block ${status}`;
-        this.barClass = `bar ${status}`;
-        this.barStyle = `width: ${this.daysOnMarket / 90 * 100}%`;
-        this.formattedDateListed = new Date(property.Date_Listed__c).toLocaleString("en-US", {month: "short", year: "numeric", day: "numeric"});
+        this.recordId = evt.detail.Id;
     }
 }
