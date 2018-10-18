776610c1574c86c099a75069ea217aa928951207
Dinko Bajric <dbajric@salesforce.com>
2018-10-18 12:15:46 -0700

    Converted Property Summary to use @wire

--- force-app/main/default/lightningcomponents/propertySummary/propertySummary.js
+++ force-app/main/default/lightningcomponents/propertySummary/propertySummary.js
@@ -1,17 +1,50 @@
-import { LightningElement, api, track} from "lwc";
+import { LightningElement, api, track, wire } from "lwc";
 import { NavigationMixin } from "lightning/navigation";
+import { showToast } from "lightning/notificationsLibrary";
+import { getRecord } from "lightning/uiRecordApi";
 
 // TODO W-5180125 - temporary workaround for mixins
 import tmpl from "./propertySummary.html";
 
+const fields = [
+    "Property__c.Id",
+    "Property__c.Thumbnail__c",
+    "Property__c.Address__c",
+    "Property__c.City__c",
+    "Property__c.State__c",
+    "Property__c.Zip__c",
+    "Property__c.Beds__c",
+    "Property__c.Baths__c",
+    "Property__c.Price__c",
+    "Property__c.Broker__r.Id",
+    "Property__c.Broker__r.Picture__c",
+    "Property__c.Broker__r.Name",
+    "Property__c.Broker__r.Title__c",
+    "Property__c.Broker__r.Mobile_Phone__c",
+    "Property__c.Broker__r.Email__c"
+];
+
 export default class PropertySummary extends NavigationMixin(LightningElement) {
-    @api property;
-    @track hasProperty = false;
-    @track hasPropertyThumbnail = false;
-    @track hasBrokerPicture = false;
+    @api recordId;
+    @track property;
+    @track broker;
 
     render() {
         return tmpl;
+    }
+
+    @wire(getRecord, { recordId: "$recordId", fields: fields })
+    wiredRecord({ error, data }) {
+        if (error) {
+            showToast({
+                title: "Error loading selected Property.",
+                message: error.message,
+                variant: "error"
+            });
+        } else if (data) {
+            this.property = data.fields;
+            this.broker = data.fields.Broker__r.value.fields;
+        }
     }
 
     connectedCallback() {
@@ -28,12 +61,7 @@ export default class PropertySummary extends NavigationMixin(LightningElement) {
      * @param {Event} evt change event.
      */
     onPropertySelected(evt) {
-        this.property = evt.detail;
-        this.hasProperty = !!this.property;
-        if(this.property) {
-            this.hasPropertyThumbnail = !!this.property.Thumbnail__c;
-            this.hasBrokerPicture = !!this.property.Broker__r.Picture__c;
-        }
+        this.recordId = evt.detail.Id;
     }
 
     /**
@@ -62,7 +90,7 @@ export default class PropertySummary extends NavigationMixin(LightningElement) {
         this[NavigationMixin.Navigate]({
             type: "standard__recordPage",
             attributes: {
-                recordId: this.property.Broker__r.Id,
+                recordId: this.broker.Id,
                 actionName: "view",
             },
         });
