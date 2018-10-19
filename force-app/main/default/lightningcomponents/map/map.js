e733aaf05716f7d78a611613bbbf009e9d3016ed
Dinko Bajric <dbajric@salesforce.com>
2018-10-18 17:51:42 -0700

    Fixed map card

--- force-app/main/default/lightningcomponents/map/map.js
+++ force-app/main/default/lightningcomponents/map/map.js
@@ -9,7 +9,7 @@ export default class Map extends LightningElement {
     @api
     set location(value) {
         this._location = value;
-        this.renderMap();
+        this.centerMapAndDrawMarker(value);
     }
     get location() {
         return this._location;
@@ -24,40 +24,46 @@ export default class Map extends LightningElement {
         ]).then(this.renderMapCallback);
     }
 
-    /**
-     * Renders the map and adds markers based on properties passed into this function.
-     */
     renderMap() {
-        let L = window.L;
-
-        // If the Leaflet library is not yet loaded, we can't draw the map: return
-        if (!L) {
-            return;
-        }
-
         // Draw the map if it hasn't been drawn yet
         if (!this.map) {
             const container = document.createElement("div");
+            container.style.height = "100%";
             this.template.querySelector("div").appendChild(container);
 
-            this.map = L.map(container, {zoomControl: true}).setView([42.356045, -71.085650], 13);
+            this.map = window.L.map(container, { zoomControl: true }).setView([42.356045, -71.085650], 13);
             this.map.scrollWheelZoom.disable();
-            L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {attribution: 'Tiles © Esri'}).addTo(this.map);
+            window.L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", { attribution: "Tiles © Esri" }).addTo(this.map);
         }
 
-        if (this.location && this.location.latitude && this.location.longitude) {
-            let latLng = [this.location.latitude, this.location.longitude];
-            if (this.map.marker) {
-                this.map.marker.setLatLng(latLng);
-            } else {
-                let myIcon = L.divIcon({
-                    className: 'my-div-icon',
-                    html: '<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 52 52"><path fill="#DB4437" d="m26 2c-10.5 0-19 8.5-19 19.1 0 13.2 13.6 25.3 17.8 28.5 0.7 0.6 1.7 0.6 2.5 0 4.2-3.3 17.7-15.3 17.7-28.5 0-10.6-8.5-19.1-19-19.1z m0 27c-4.4 0-8-3.6-8-8s3.6-8 8-8 8 3.6 8 8-3.6 8-8 8z"></path></svg>'
-                });
-                this.map.marker = window.L.marker(latLng, {icon: myIcon});
-                this.map.marker.addTo(this.map.map);
-            }
-            this.map.map.setView(latLng);
+        if (this.location) {
+            this.centerMapAndDrawMarker(this.location);
         }
+    }
+
+    /**
+     * Centers the map and draws a marker at the location specified.
+     * @param {Object} location The location of the marker and center of the map.
+     * @param {Number} location.latitude The latitude coordinate of the marker
+     * @param {Number} location.longitude The longitude coordinate of the marker
+     */
+    centerMapAndDrawMarker(location) {
+        // If map is not available, we can't draw the marker. Skip for now.
+        if (!this.map) {
+            return;
+        }
+
+        const latLng = [location.latitude, location.longitude];
+        if (this.map.marker) {
+            this.map.marker.setLatLng(latLng);
+        } else {
+            let myIcon = L.divIcon({
+                className: "my-div-icon",
+                html: '<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 52 52"><path fill="#DB4437" d="m26 2c-10.5 0-19 8.5-19 19.1 0 13.2 13.6 25.3 17.8 28.5 0.7 0.6 1.7 0.6 2.5 0 4.2-3.3 17.7-15.3 17.7-28.5 0-10.6-8.5-19.1-19-19.1z m0 27c-4.4 0-8-3.6-8-8s3.6-8 8-8 8 3.6 8 8-3.6 8-8 8z"></path></svg>'
+            });
+            this.map.marker = window.L.marker(latLng, { icon: myIcon });
+            this.map.marker.addTo(this.map);
+        }
+        this.map.setView(latLng);
     }
 }
--- force-app/main/default/lightningcomponents/mapCard/mapCard.js
+++ force-app/main/default/lightningcomponents/mapCard/mapCard.js
@@ -1,74 +1,36 @@
-import { LightningElement, track, api } from "lwc";
-import { loadStyle, loadScript } from "c/utils";
-import leaflet from "@salesforce/resourceUrl/leaflet";
+import { LightningElement, track, api, wire } from "lwc";
+import { getRecord } from "lightning/uiRecordApi";
+
+const fields = ["Property__c.Name", "Property__c.Location__Latitude__s", "Property__c.Location__Longitude__s"];
 
 export default class MapCard extends LightningElement {
+    @api recordId;
     @track title;
     @track isFullScreen = false;
-    @track location = { longitude: 42.356045, latitude: -71.085650 };
+    @track location = { latitude: 42.356045, longitude: -71.085650 };
 
-    renderedCallback() {
-        //this.renderMapCallback = this.renderMap.bind(this);
-
-        Promise.all([
-            loadStyle(document, leaflet + "/leaflet.css"),
-            loadScript(document, leaflet + "/leaflet.js")
-        ]).then(this.renderMapCallback);
+    @wire(getRecord, { recordId: "$recordId", fields: fields })
+    wiredRecord({ error, data }) {
+        if (error) {
+            showToast({
+                title: "Error loading pictures",
+                message: error.message,
+                variant: "error"
+            });
+        } else if (data) {
+            this.title = data.fields.Name.value;
+            this.location = {
+                latitude: data.fields.Location__Latitude__s.value,
+                longitude: data.fields.Location__Longitude__s.value
+            };
+        }
     }
 
-    onFullScreen(evt) {
+    onFullScreen() {
         this.isFullScreen = true;
-        // var sObject = component.get("v.sObject");
-        // if (sObject) {
-        //     component.find("bigMap").setLocation(sObject[component.get("v.latField")], sObject[component.get("v.longField")]);
-        // }
     }
 
     closeDialog() {
         this.isFullScreen = false;
     }
 }
-
-
-/* // TODO
-({
-    doInit : function(component) {
-        component.set("v.fields", ["Id", component.get("v.latField"), component.get("v.longField"), component.get("v.titleField")]);
-        var recordId = component.get("v.recordId");
-        component.set("v.dsRecordId", recordId);
-        var service = component.find("service");
-        service.reloadRecord();
-	},
-
-    fullScreen : function(component) {
-        console.log(component.get("v.property"));
-        component.set("v.fullScreen", true);
-		var sObject = component.get("v.sObject");
-        if (sObject) {
-            component.find("bigMap").setLocation(sObject[component.get("v.latField")], sObject[component.get("v.longField")]);
-        }
-
-	},
-
-	closeDialog : function(component) {
-        component.set("v.fullScreen", false);
-	},
-
-  	recordChangeHandler : function(component, event) {
-        console.log('recordChangeHandler');
-        var id = event.getParam("recordId");
-        component.set("v.dsRecordId", id);
-        var service = component.find("service");
-        service.reloadRecord();
-	},
-
-  	onRecordUpdated : function(component, event) {
-        console.log('onRecordUpdated');
-        var sObject = component.get("v.sObject");
-        if (sObject) {
-	        component.set("v.title", sObject[component.get("v.titleField")]);
-            component.find("map").setLocation(sObject[component.get("v.latField")], sObject[component.get("v.longField")]);
-        }
-    }
-})
-*/
